f6b00eb4895336bcf84063d4d358d593a8440df4
ziting0426 <125638863+ziting0426@users.noreply.github.com>
2023-03-01 14:04:17 +0800

    Add files via upload

---
+++ A1113306_0224_2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A1113306_0224_2{
+	public static void main(String[] args){
+		double degree;
+		double degree2;
+		System.out.print("輸入(度C):");
+		Scanner sc=new Scanner(System.in);
+		degree=sc.nextDouble();
+		degree2=(degree*9/5)+32;
+		System.out.println(degree2+"度F");	
+	}
+}
